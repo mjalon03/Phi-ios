75461c2a73d5c8915f90cd078309adcc42b5c78d
mck0124 <minchan0124@gmail.com>
2025-10-28 19:25:50 +0800

    Initial Commit

---
+++ Citizen_Alerts/Citizen_AlertsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Citizen_AlertsApp.swift
+//  Citizen_Alerts
+//
+//  Created by Minchan Kim on 10/28/25.
+//
+
+import SwiftUI
+
+@main
+struct Citizen_AlertsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
